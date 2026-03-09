9cb63731b7262a99e7962fa601753053ab28b8da
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2026-03-09 18:44:19 +0100

    Release v4.26.0

--- src_Set-OutlookSignatures/Set-OutlookSignatures.ps1
+++ src_Set-OutlookSignatures/Set-OutlookSignatures.ps1
@@ -10416,10 +10416,8 @@ function ParseJwtToken {
 #   try { global:WatchCatchableExitSignal -CleanupDone } catch {}
 #
 
-$global:WatchCatchableExitSignalNextCheck = [environment]::TickCount64
 $global:WatchCatchableExitSignalStatus = [hashtable]::Synchronized(@{})
 $global:WatchCatchableExitSignalStatus[0] = 'Nothing detected yet'
-
 # Possible values for $global:WatchCatchableExitSignalStatus[0]
 #   "Nothing detected yet" when no catchable exit signal has been found until now
 #   "Detected '<description>', initiate clean-up and exit" when a catchable exit signal has been found
@@ -10459,16 +10457,11 @@ if ((-not (Test-Path -LiteralPath 'variable:IsWindows')) -or $IsWindows) {
                 }
 
                 protected override void WndProc(ref Message m) {
-                    bool isQueryEnd = (m.Msg == 17);
-                    bool isEndSessionWithParam = (m.Msg == 22 && m.WParam != IntPtr.Zero);
-
-                    if (isQueryEnd || isEndSessionWithParam) {
-                        if (EndSessionInitiateCleanup != null && !this.IsDisposed) {
-                            try {
-                                EndSessionInitiateCleanup.Invoke(m);
-                            } catch {
-                                // Fail silently to avoid crashing the message pump
-                            }
+                    if (EndSessionInitiateCleanup != null && !this.IsDisposed) {
+                        try {
+                            EndSessionInitiateCleanup.Invoke(m);
+                        } catch {
+                            // Do nothing
                         }
                     }
 
@@ -10499,24 +10492,32 @@ if ((-not (Test-Path -LiteralPath 'variable:IsWindows')) -or $IsWindows) {
                     param($message)
 
                     try {
-                        # Logoff/Reboot/Shutdown will happen.
-                        # Set status, wait for clean-up and then return 0.
-
-                        $m = $message.Msg
-
-                        $global:WatchCatchableExitSignalStatus[0] = "Detected '$($m), $(if ($m -eq 17) { 'WM_QUERYENDSESSION' } else { 'WM_ENDSESSION' }), $($message.WParam), $($message.LParam)', initiate clean-up and exit"
-
-                        until (
-                            $($global:WatchCatchableExitSignalStatus[0] -eq 'Clean-up done')
-                        ) {
-                            Start-Sleep -Milliseconds 100
+                        $WindowsMessagesByDecimal = @{
+                            0 = 'WM_NULL'; 2 = 'WM_DESTROY'; 3 = 'WM_MOVE'; 5 = 'WM_SIZE'; 6 = 'WM_ACTIVATE'; 7 = 'WM_SETFOCUS'; 8 = 'WM_KILLFOCUS'; 10 = 'WM_ENABLE'; 11 = 'WM_SETREDRAW'; 12 = 'WM_SETTEXT'; 13 = 'WM_GETTEXT'; 14 = 'WM_GETTEXTLENGTH'; 15 = 'WM_PAINT'; 16 = 'WM_CLOSE'; 17 = 'WM_QUERYENDSESSION'; 18 = 'WM_QUIT'; 19 = 'WM_QUERYOPEN'; 20 = 'WM_ERASEBKGND'; 21 = 'WM_SYSCOLORCHANGE'; 22 = 'WM_ENDSESSION'; 23 = 'WM_SYSTEMERROR'; 24 = 'WM_SHOWWINDOW'; 25 = 'WM_CTLCOLOR'; 26 = 'WM_SETTINGCHANGE'; 27 = 'WM_DEVMODECHANGE'; 28 = 'WM_ACTIVATEAPP'; 29 = 'WM_FONTCHANGE'; 30 = 'WM_TIMECHANGE'; 31 = 'WM_CANCELMODE'; 32 = 'WM_SETCURSOR'; 33 = 'WM_MOUSEACTIVATE'; 34 = 'WM_CHILDACTIVATE'; 35 = 'WM_QUEUESYNC'; 36 = 'WM_GETMINMAXINFO'; 38 = 'WM_PAINTICON'; 39 = 'WM_ICONERASEBKGND'; 40 = 'WM_NEXTDLGCTL'; 42 = 'WM_SPOOLERSTATUS'; 43 = 'WM_DRAWITEM'; 44 = 'WM_MEASUREITEM'; 45 = 'WM_DELETEITEM'; 46 = 'WM_VKEYTOITEM'; 47 = 'WM_CHARTOITEM'; 48 = 'WM_SETFONT'; 49 = 'WM_GETFONT'; 50 = 'WM_SETHOTKEY'; 51 = 'WM_GETHOTKEY'; 55 = 'WM_QUERYDRAGICON'; 57 = 'WM_COMPAREITEM'; 65 = 'WM_COMPACTING'; 70 = 'WM_WINDOWPOSCHANGING'; 71 = 'WM_WINDOWPOSCHANGED'; 72 = 'WM_POWER'; 74 = 'WM_COPYDATA'; 75 = 'WM_CANCELJOURNAL'; 78 = 'WM_NOTIFY'; 80 = 'WM_INPUTLANGCHANGEREQUEST'; 81 = 'WM_INPUTLANGCHANGE'; 82 = 'WM_TCARD'; 83 = 'WM_HELP'; 84 = 'WM_USERCHANGED'; 85 = 'WM_NOTIFYFORMAT'; 123 = 'WM_CONTEXTMENU'; 124 = 'WM_STYLECHANGING'; 125 = 'WM_STYLECHANGED'; 126 = 'WM_DISPLAYCHANGE'; 127 = 'WM_GETICON'; 128 = 'WM_SETICON'; 129 = 'WM_NCCREATE'; 130 = 'WM_NCDESTROY'; 131 = 'WM_NCCALCSIZE'; 132 = 'WM_NCHITTEST'; 133 = 'WM_NCPAINT'; 134 = 'WM_NCACTIVATE'; 135 = 'WM_GETDLGCODE'; 160 = 'WM_NCMOUSEMOVE'; 161 = 'WM_NCLBUTTONDOWN'; 162 = 'WM_NCLBUTTONUP'; 163 = 'WM_NCLBUTTONDBLCLK'; 164 = 'WM_NCRBUTTONDOWN'; 165 = 'WM_NCRBUTTONUP'; 166 = 'WM_NCRBUTTONDBLCLK'; 167 = 'WM_NCMBUTTONDOWN'; 168 = 'WM_NCMBUTTONUP'; 169 = 'WM_NCMBUTTONDBLCLK'; 256 = 'WM_KEYDOWN'; 257 = 'WM_KEYUP'; 258 = 'WM_CHAR'; 259 = 'WM_DEADCHAR'; 260 = 'WM_SYSKEYDOWN'; 261 = 'WM_SYSKEYUP'; 262 = 'WM_SYSCHAR'; 263 = 'WM_SYSDEADCHAR'; 264 = 'WM_KEYLAST'; 269 = 'WM_IME_STARTCOMPOSITION'; 270 = 'WM_IME_ENDCOMPOSITION'; 271 = 'WM_IME_COMPOSITION'; 272 = 'WM_INITDIALOG'; 273 = 'WM_COMMAND'; 274 = 'WM_SYSCOMMAND'; 275 = 'WM_TIMER'; 276 = 'WM_HSCROLL'; 277 = 'WM_VSCROLL'; 278 = 'WM_INITMENU'; 279 = 'WM_INITMENUPOPUP'; 287 = 'WM_MENUSELECT'; 288 = 'WM_MENUCHAR'; 289 = 'WM_ENTERIDLE'; 306 = 'WM_CTLCOLORMSGBOX'; 307 = 'WM_CTLCOLOREDIT'; 308 = 'WM_CTLCOLORLISTBOX'; 309 = 'WM_CTLCOLORBTN'; 310 = 'WM_CTLCOLORDLG'; 311 = 'WM_CTLCOLORSCROLLBAR'; 312 = 'WM_CTLCOLORSTATIC'; 512 = 'WM_MOUSEMOVE'; 513 = 'WM_LBUTTONDOWN'; 514 = 'WM_LBUTTONUP'; 515 = 'WM_LBUTTONDBLCLK'; 516 = 'WM_RBUTTONDOWN'; 517 = 'WM_RBUTTONUP'; 518 = 'WM_RBUTTONDBLCLK'; 519 = 'WM_MBUTTONDOWN'; 520 = 'WM_MBUTTONUP'; 521 = 'WM_MBUTTONDBLCLK'; 522 = 'WM_MOUSEWHEEL'; 526 = 'WM_MOUSEHWHEEL'; 528 = 'WM_PARENTNOTIFY'; 529 = 'WM_ENTERMENULOOP'; 530 = 'WM_EXITMENULOOP'; 531 = 'WM_NEXTMENU'; 532 = 'WM_SIZING'; 533 = 'WM_CAPTURECHANGED'; 534 = 'WM_MOVING'; 536 = 'WM_POWERBROADCAST'; 537 = 'WM_DEVICECHANGE'; 544 = 'WM_MDICREATE'; 545 = 'WM_MDIDESTROY'; 546 = 'WM_MDIACTIVATE'; 547 = 'WM_MDIRESTORE'; 548 = 'WM_MDINEXT'; 549 = 'WM_MDIMAXIMIZE'; 550 = 'WM_MDITILE'; 551 = 'WM_MDICASCADE'; 552 = 'WM_MDIICONARRANGE'; 553 = 'WM_MDIGETACTIVE'; 560 = 'WM_MDISETMENU'; 561 = 'WM_ENTERSIZEMOVE'; 562 = 'WM_EXITSIZEMOVE'; 563 = 'WM_DROPFILES'; 564 = 'WM_MDIREFRESHMENU'; 641 = 'WM_IME_SETCONTEXT'; 642 = 'WM_IME_NOTIFY'; 643 = 'WM_IME_CONTROL'; 644 = 'WM_IME_COMPOSITIONFULL'; 645 = 'WM_IME_SELECT'; 646 = 'WM_IME_CHAR'; 656 = 'WM_IME_KEYDOWN'; 657 = 'WM_IME_KEYUP'; 673 = 'WM_MOUSEHOVER'; 674 = 'WM_NCMOUSELEAVE'; 675 = 'WM_MOUSELEAVE'; 768 = 'WM_CUT'; 769 = 'WM_COPY'; 770 = 'WM_PASTE'; 771 = 'WM_CLEAR'; 772 = 'WM_UNDO'; 773 = 'WM_RENDERFORMAT'; 774 = 'WM_RENDERALLFORMATS'; 775 = 'WM_DESTROYCLIPBOARD'; 776 = 'WM_DRAWCLIPBOARD'; 777 = 'WM_PAINTCLIPBOARD'; 778 = 'WM_VSCROLLCLIPBOARD'; 779 = 'WM_SIZECLIPBOARD'; 780 = 'WM_ASKCBFORMATNAME'; 781 = 'WM_CHANGECBCHAIN'; 782 = 'WM_HSCROLLCLIPBOARD'; 783 = 'WM_QUERYNEWPALETTE'; 784 = 'WM_PALETTEISCHANGING'; 785 = 'WM_PALETTECHANGED'; 786 = 'WM_HOTKEY'; 791 = 'WM_PRINT'; 792 = 'WM_PRINTCLIENT'; 856 = 'WM_HANDHELDFIRST'; 863 = 'WM_HANDHELDLAST'; 896 = 'WM_PENWINFIRST'; 911 = 'WM_PENWINLAST'; 912 = 'WM_COALESCE_FIRST'; 927 = 'WM_COALESCE_LAST'; 992 = 'WM_DDE_INITIATE'; 993 = 'WM_DDE_TERMINATE'; 994 = 'WM_DDE_ADVISE'; 995 = 'WM_DDE_UNADVISE'; 996 = 'WM_DDE_ACK'; 997 = 'WM_DDE_DATA'; 998 = 'WM_DDE_REQUEST'; 999 = 'WM_DDE_POKE'; 1000 = 'WM_DDE_EXECUTE'
                         }
 
-                        $message.Result = [IntPtr]::Zero
+                        if (
+                            $(
+                                $($WindowsMessagesByDecimal[$($message.Msg)] -ieq 'WM_ENDSESSION') -and
+                                $($message.WParam -ne [IntPtr]::Zero)
+                            ) -or
+                            $($WindowsMessagesByDecimal[$($message.Msg)] -ieq 'WM_QUERYENDSESSION')
+                        ) {
+                            # Logoff/Reboot/Shutdown will happen.
+                            # Set status, wait for clean-up and then return 0.
+                            $global:WatchCatchableExitSignalStatus[0] = "Detected '$(@(@($($message.Msg), $($WindowsMessagesByDecimal[$($message.Msg)]), $($message.WParam), $($message.LParam)) | Where-Object {$_})-join ', ')', initiate clean-up and exit"
 
-                        $formRef.Value.Dispose()
+                            until (
+                                $($global:WatchCatchableExitSignalStatus[0] -eq 'Clean-up done')
+                            ) {
+                                Start-Sleep -Milliseconds 100
+                            }
+
+                            $message.Result = [IntPtr]::Zero
+
+                            $formRef.Value.Close()
+                        }
                     } catch {
-                        # Do nothing
                     }
                 }
             )
@@ -10552,25 +10553,29 @@ function global:WatchCatchableExitSignal {
         [switch]$CleanupDone
     )
 
-    if ([environment]::TickCount64 -lt $global:WatchCatchableExitSignalNextCheck) {
-        return
-    }
-
-    $global:WatchCatchableExitSignalNextCheck = [environment]::TickCount64 + 500
-
-    $statusRef = $global:WatchCatchableExitSignalStatus[0]
-
     if ($CleanupDone) {
-        if ($null -ne $WatchCatchableExitSignalForm) {
-            try { $WatchCatchableExitSignalForm.Dispose() } catch {}
+        if ($WatchCatchableExitSignalForm) {
+            try {
+                $WatchCatchableExitSignalForm.Close()
+            } catch {
+                # Do nothing
+            }
         }
-        $global:WatchCatchableExitSignalStatus[0] = 'Clean-up done'
-    } elseif ($statusRef -like "Detected '*', initiate clean-up and exit") {
-        Write-Host
-        Write-Host "WatchCatchableExitSignal: $statusRef" -ForegroundColor Yellow
 
-        if ($null -ne $WatchCatchableExitSignalForm) {
-            try { $WatchCatchableExitSignalForm.Dispose() } catch {}
+        $global:WatchCatchableExitSignalStatus[0] = 'Clean-up done'
+    } elseif (
+        $global:WatchCatchableExitSignalStatus[0].StartsWith('Detected ''') -and
+        $global:WatchCatchableExitSignalStatus[0].EndsWith(''', initiate clean-up and exit')
+    ) {
+        Write-Host
+        Write-Host "WatchCatchableExitSignal: $($global:WatchCatchableExitSignalStatus[0])" -ForegroundColor Yellow
+
+        if ($WatchCatchableExitSignalForm) {
+            try {
+                $WatchCatchableExitSignalForm.Close()
+            } catch {
+                # Do nothing
+            }
         }
 
         $script:ExitCode = 1
